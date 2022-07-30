f24c8b011c5baca1991c44fb7f824537956b7fa0
admin <parthasarathi@datacontech.com>
2022-07-30 16:20:34 +0530

    first commit

--- UserMgmt/AccessTypeFrom.aspx.cs
+++ UserMgmt/AccessTypeFrom.aspx.cs
@@ -11,6 +11,7 @@ using Usermngt.Entities;
 
 namespace UserMgmt
 {
+    //Checking github
     public partial class AccessTypeFrom : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
